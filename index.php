53a0cf45d9b047c8e89b56496d64975bfd72983c
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-02-25 13:05:23 +0300

    refactored for new received data

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ if(isset($_GET['url']) && $_GET['url'] != ""){
     parse_str( parse_url( $_GET['url'], PHP_URL_QUERY ), $vars );
     
     $id=$vars['v'];
-    $dt=file_get_contents("http://www.youtube.com/get_video_info?video_id=$id&el=embedded&ps=default&eurl=&gl=US&hl=en");
-    //var_dump(explode("&",$dt));
+    $dt=file_get_contents("https://www.youtube.com/get_video_info?video_id=$id&el=embedded&ps=default&eurl=&gl=US&hl=en");
+    // var_dump(explode("&",$dt));
     if (strpos($dt, 'status=fail') !== false) {
         
         $x=explode("&",$dt);
@@ -35,7 +35,8 @@ if(isset($_GET['url']) && $_GET['url'] != ""){
         echo json_encode($g,JSON_PRETTY_PRINT);
         
     }else{
-        
+        // $a = json_decode(urldecode(explode("=",explode("&",$dt)[13])[1]))->streamingData->formats;
+        // echo json_encode($a);
         $x=explode("&",$dt);
         $t=array(); $g=array(); $h=array();
         foreach($x as $r){
@@ -44,18 +45,19 @@ if(isset($_GET['url']) && $_GET['url'] != ""){
             $y=urldecode($v);
             $t[$n]=$v;
         }
-        $h=[];
-        $streams = explode(',',urldecode($t['url_encoded_fmt_stream_map']));
-        foreach($streams as $dt){ 
-            $x=explode("&",$dt);
-            foreach($x as $r){
-                $c=explode("=",$r);
-                $n=$c[0]; $v=$c[1];
-                $h[$n]=urldecode($v);
-            }
-            $g[]=$h;
-        }
-        echo json_encode($g,JSON_PRETTY_PRINT);
+        // echo json_encode(json_decode(urldecode($t['player_response']))->streamingData->formats[0]);
+        $streams = json_decode(urldecode($t['player_response']))->streamingData->formats;
+        // echo json_encode($streams);
+        // foreach($streams as $d){ 
+        //     $x=explode("&",$d);
+        //     foreach($x as $r){
+        //         $c=explode("=",$r);
+        //         $n=$c[0]; $v=$c[1];
+        //         $h[$n]=urldecode($v);
+        //     }
+        //     $g[]=$h;
+        // }
+        echo json_encode($streams);
        // var_dump( $g[1]["quality"],true);
     }
 }else{
@@ -67,3 +69,4 @@ if(isset($_GET['url']) && $_GET['url'] != ""){
     $myJSON = json_encode($myObj,JSON_PRETTY_PRINT);
     echo $myJSON;
 }
+
